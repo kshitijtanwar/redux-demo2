12a7189b94c2e1146aab1c0a9cf3ce46bdadde31
Kshitij <kshitij.tanwar2002@gmail.com>
2023-10-15 01:26:38 +0530

    replaceCart

--- src/Store/cart-items.jsx
+++ src/Store/cart-items.jsx
@@ -8,6 +8,10 @@ const cartItemSlice = createSlice({
     name: "cart",
     initialState: cartItemInitialState,
     reducers: {
+        replaceCart(state, action) {
+            state.totalQuantity = action.payload.totalQuantity;
+            state.items = action.payload.items;
+        },
         addItem(state, action) {
             const newItem = action.payload;
             const existingItem = state.items.find(
@@ -30,15 +34,14 @@ const cartItemSlice = createSlice({
         },
         removeItem(state, action) {
             const id = action.payload;
-            const existingItem = state.items.find(
-                (item) => item.id === id
-            );
+            const existingItem = state.items.find((item) => item.id === id);
             state.totalQuantity--;
-            if(existingItem.quantity === 1){
-                state.items = state.items.filter(item => item.id !== id);
-            }else{
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter((item) => item.id !== id);
+            } else {
                 existingItem.quantity--;
-                existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+                existingItem.totalPrice =
+                    existingItem.totalPrice - existingItem.price;
             }
         },
     },
